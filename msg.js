6c0d8da3fc589e0d75d7bc6ae3ec59511ae2d368
hel00322 <heloisatavaresn22@gmail.com>
2024-08-12 15:29:40 -0300

    up

--- msg.js
+++ msg.js
@@ -2,5 +2,4 @@ document.getElementById("enviarMsg").addEventListener("click", function(event) {
     event.preventDefault(); // Previne o envio imediato do formulário
     console.log("Botão clicado!"); // Verifica se o clique está sendo registrado
     alert("Mensagem enviada com sucesso!"); // Exibe o alerta
-    document.querySelector("form").submit(); // Submete o formulário manualmente
 });
