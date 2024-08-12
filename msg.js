205f8013c7e5ca6ad9e2c722f671dc37e5f96b35
hel00322 <heloisatavaresn22@gmail.com>
2024-08-12 15:34:08 -0300

    up

--- msg.js
+++ msg.js
@@ -2,4 +2,5 @@ document.getElementById("enviarMsg").addEventListener("click", function(event) {
     event.preventDefault(); // Previne o envio imediato do formulário
     console.log("Botão clicado!"); // Verifica se o clique está sendo registrado
     alert("Mensagem enviada com sucesso!"); // Exibe o alerta
+    document.querySelector("form").submit('https://guloseimasdobem.netlify.app/contato'); // Submete o formulário manualmente
 });
